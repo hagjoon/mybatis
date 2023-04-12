87e66c1b3a6d6ca02270f27aef6c45cc8ef26dda
choihagjoon <gkrwns6307@gmail.com>
2023-04-12 15:24:59 +0900

    [ksmart46] 깃 허브 연동 테스트 author: 최학준 file: 초기 파일 셋팅 detail: 깃 허브 프로젝트 초기 셋팅

--- src/main/java/ksmart/mybatis/interceptor/LoginInterceptor.java
+++ src/main/java/ksmart/mybatis/interceptor/LoginInterceptor.java
@@ -19,6 +19,7 @@ public class LoginInterceptor implements HandlerInterceptor {
     public LoginInterceptor(MemberMapper memberMapper){
         this.memberMapper = memberMapper;
     }
+    //hi
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
